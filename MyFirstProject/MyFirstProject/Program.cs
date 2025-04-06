9b6d29d714e876ee7b648dbf2d457aad0c11bc76
DavorSopar <145041572+DavorSopar@users.noreply.github.com>
2025-04-06 22:42:32 +0200

    Add files via upload

---
+++ MyFirstProject/MyFirstProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 23;
+
+            Console.WriteLine(age);
+
+            long bigNumber = 900000000L;
+            Console.ReadLine();
+        }
+    }
+}
